c3e026ac94a7317783de6acf41772975dc1a2724
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-12 16:19:27 +0200

    Initial Commit

---
+++ Shared/StorageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageAppApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 12/09/21.
+//
+
+import SwiftUI
+
+@main
+struct StorageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
