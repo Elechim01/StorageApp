25a64cdedd0be5681ca80eb4e6c4b3d6c3b1d57f
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-12 22:48:59 +0200

    primo commit

--- Shared/StorageAppApp.swift
+++ Shared/StorageAppApp.swift
@@ -10,8 +10,26 @@ import SwiftUI
 @main
 struct StorageAppApp: App {
     var body: some Scene {
+        #if os(macOS)
         WindowGroup {
             ContentView()
         }
+//        Hidding title Bar..
+        .windowStyle(HiddenTitleBarWindowStyle())
+        #else
+        WindowGroup {
+            ContentView()
+        }
+        #endif
     }
 }
+
+#if os(macOS)
+//Hidding Focus Ring...
+extension NSTextField{
+    open override var focusRingType: NSFocusRingType{
+        get{.none}
+        set{}
+    }
+}
+#endif
---
+++ Shared/View/MainContent.swift
@@ -0,0 +1,280 @@
+//
+//  MainContent.swift
+//  MainContent
+//
+//  Created by Michele Manniello on 12/09/21.
+//
+
+import SwiftUI
+
+struct MainContent: View {
+    @State var search = ""
+    @Binding var showMenu : Bool
+    var body: some View {
+        VStack{
+            HStack {
+                if !isMacOS(){
+                    Button {
+                        withAnimation {
+                            showMenu.toggle()
+                        }
+                    } label: {
+                        Image(systemName: "line.horizontal.3")
+                            .font(.title)
+                            .foregroundColor(.black)
+                    }
+
+                }
+                Text("Dashboard")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                
+                Spacer(minLength: 0)
+                
+                if isMacOS(){
+                    //                Custom Search Bar...
+                    CustomSearchBar(search: $search)
+                        .frame(maxWidth: 300)
+                }
+            }
+            .padding(isMacOS() ? 15 : 0 )
+            
+            if !isMacOS(){
+                CustomSearchBar(search: $search)
+                    .padding(.bottom)
+            }
+            
+            ZStack{
+                if isMacOS(){
+                    ScrollView(.vertical,showsIndicators: false){
+                        BodyContent()
+                    }
+                }else{
+                    BodyContent()
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color("gray"))
+            .cornerRadius(isMacOS() ? 10 : 0)
+            .padding(isMacOS() ? 10 : 0)
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+    }
+}
+
+struct MainContent_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct BodyContent: View {
+    var body: some View{
+        VStack{
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 15){
+                    StorageView(image: "dropbox", title: "Dropbox", percentage: 0.6)
+                    StorageView(image: "drive", title: "Google Drive", percentage: 0.25)
+                    StorageView(image: "icloud", title: "iCloud", percentage: 0.9)
+                }
+            }
+            
+            Text("Quick Access")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+                .padding(.top)
+                .frame(maxWidth: .infinity, alignment: .leading)
+//                    Quick Access Buttons...
+            ScrollView(.horizontal,showsIndicators: false) {
+                HStack(spacing: 0){
+                    AccessButton(title: "Pictures", image: "photo", color: .yellow)
+                    
+                    AccessButton(title: "Music", image: "music.note.house.fill", color: .blue)
+                    
+                    AccessButton(title: "Videos", image: "play.rectangle.fill", color: .red)
+                    
+                    AccessButton(title: "Apps", image: "square.grid.2x2.fill", color: .yellow)
+                    
+                    AccessButton(title: "Documents", image: "doc.fill", color: .blue)
+                    
+                    AccessButton(title: "Downloads", image: "arrow.down.app.fill", color: .pink)
+
+                }
+                .padding(.top)
+            }
+            
+            Text("Recent Files")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+                .padding(.top,20)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack{
+                    VStack{
+                        HStack{
+                            Text("Name")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                            Spacer(minLength: 0)
+                            Text("Size")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                        Divider()
+                        VStack(spacing: 12){
+//                                Files...
+                        FilesView(fileName: "Michele.jpg", fileSize: "8 MB", fileImage: "photo")
+                        
+                        FilesView(fileName: "Apple.m4p", fileSize: "8 MB", fileImage: "play.rectangle.fill")
+                        
+                        FilesView(fileName: "WWDC.jpg", fileSize: "8 MB", fileImage: "photo")
+                        
+                        FilesView(fileName: "SwiftUI 3.0.jpg", fileSize: "8 MB", fileImage: "play.rectangle.fill")
+                        }
+                    }
+                    .padding()
+//                            max Width...
+                    .frame(width: 250)
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 5, y: 5)
+                }
+                .padding(.top)
+            }
+        }
+        .padding(isMacOS() ? 15 : 0)
+    }
+}
+
+struct StorageView: View {
+    
+    var image : String
+    var title : String
+    var percentage : CGFloat
+    
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            HStack{
+                Image(image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 45, height: 45)
+                Divider()
+                    .frame(height: 45)
+                    .padding(.horizontal)
+                ZStack{
+                    Circle()
+                        .stroke(Color.gray.opacity(0.4),lineWidth: 8)
+                    Circle()
+                        .trim(from: 0, to: percentage)
+                        .stroke(Color("blue"),style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
+                        .rotationEffect(.init(degrees: -90))
+                    
+                    Text("\(Int(percentage * 100))%")
+                }
+                .frame(width: 50, height: 50)
+            }
+            Text(title)
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+                .padding(.top)
+            Text("143 GB / 150 GB")
+                .font(.caption)
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+    }
+}
+
+struct AccessButton: View {
+    
+    var title : String
+    var image : String
+    var color : Color
+    
+    var body: some View {
+        Button {
+            
+        } label: {
+            VStack(spacing: 12){
+                Image(systemName: image)
+                    .font(.title)
+                    .foregroundColor(color)
+                //                                    Square Shape Background..
+                    .frame(width: 50, height: 50)
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 5, y: 5)
+                Text(title)
+                    .font(.caption)
+                    .fontWeight(.bold)
+                    .kerning(1.3)
+                    .foregroundColor(.black)
+            }
+            .frame(width: isMacOS() ?  80 : 100 )
+        }
+    }
+}
+
+struct FilesView: View {
+    
+    var fileName: String
+    var fileSize : String
+    var fileImage : String
+    
+    var body: some View {
+        HStack(spacing: 12){
+            Image(systemName: fileImage)
+                .foregroundColor(fileImage == "photo" ? .yellow : .pink)
+                .padding(6)
+                .background(RoundedRectangle(cornerRadius: 4).stroke(Color.gray.opacity(0.3)))
+            VStack(alignment: .leading, spacing: 4) {
+                Text(fileName)
+                    .font(.system(size: 15))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                Text("16 Dicembre 2001")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            Spacer(minLength: 10)
+            
+            Text(fileSize)
+                .font(.caption)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+        }
+    }
+}
+
+struct CustomSearchBar: View {
+    @Binding var search : String
+    var body: some View {
+        HStack{
+            TextField("Search", text: $search)
+                .textFieldStyle(PlainTextFieldStyle())
+            Button {
+                
+            } label: {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.white)
+                    .padding(8)
+                    .background(Color("blue"))
+                    .cornerRadius(4)
+            }
+        }
+        .padding(.leading)
+        .background(Color.white)
+        .cornerRadius(8)
+        .shadow(color: Color.black.opacity(0.05), radius: 5, x: 5, y: 5)
+        .shadow(color: Color.black.opacity(0.05), radius: 5, x: -5, y: -5)
+    }
+}
